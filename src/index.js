a36e36e83faea1361dc7e9528268317b49e29d08
SumaiaSorna <sorna.sumaia@gmail.com>
2021-12-06 12:51:20 +0000

    initial commit with files and folders

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Include packages needed for this application
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const con = mysql.createConnection({ database: "" ,user:});
+
