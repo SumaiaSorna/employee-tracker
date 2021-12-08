ff23555bf2d38a54da6f7005ec0db989382edafb
SumaiaSorna <sorna.sumaia@gmail.com>
2021-12-08 13:18:03 +0000

    wip

---
+++ src/db/data.js
@@ -0,0 +1,34 @@
+const departments = ["Marketing", "Finance", "Accounting"];
+
+const role = [
+  {
+    title: "Digital Marketing Assistant",
+    salary: 45000,
+    departmentId: 1,
+  },
+  {
+    title: "Marketing Executive",
+    salary: 60000,
+    departmentId: 1,
+  },
+  {
+    title: "digital Marketing Assistant",
+    salary: 45000,
+    departmentId: 1,
+  },
+  {
+    title: "digital Marketing Assistant",
+    salary: 45000,
+    departmentId: 1,
+  },
+  {
+    title: "digital Marketing Assistant",
+    salary: 45000,
+    departmentId: 1,
+  },
+  {
+    title: "digital Marketing Assistant",
+    salary: 45000,
+    departmentId: 1,
+  },
+];
