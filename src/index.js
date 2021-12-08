0543d52f803577309e98cb4073537c1ea1853ff7
SumaiaSorna <sorna.sumaia@gmail.com>
2021-12-08 20:06:59 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 // Include packages needed for this application
-const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const questions = require("./question");
+
+const displayDepartments = () => {
+  // execute mysql query
+  // log/table departments
+};
+
+const displayRoles = () => {
+  // execute mysql query
+  // log/table roles
+};
+
+const displayEmployees = () => {
+  // execute mysql query
+  // log/table employees
+};
+
+const getDepartments = () => {
+  // execute mysql query
+  // return departments
+};
+
+const getRoles = () => {
+  // execute mysql query
+  // return roles
+};
+
+const getEmployees = () => {
+  // execute mysql query
+  // return employees
+};
+
+const constructDepartmentChoices = (departments) => {
+  // return an array of department choices
+};
+
+const constructRoleChoices = (roles) => {
+  // return an array of role choices
+};
+
+const constructEmployeeChoices = (roles) => {
+  // return an array of employee choices
+};
+
+// const db = mysql.createConnection(
+//   {
+//     host: 'localhost',
+//     // MySQL username,
+//     user: 'root',
+//     // MySQL password
+//     password: '',
+//     database: 'classlist_db',
+//   },
+//   console.log(`Connected to the classlist_db database.`)
+// );
+
+const start = () => {};
+
+start();
