95128c0627c8dd5819161e0cd9781eb349448db3
SumaiaSorna <sorna.sumaia@gmail.com>
2021-12-08 01:05:04 +0000

    update files and folders and worked on schema.sql file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 // Include packages needed for this application
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const con = mysql.createConnection({ database: "" ,user:});
-
