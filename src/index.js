c80238ac5a2004c5aea0ce8cdefceee6a41688ff
SumaiaSorna <sorna.sumaia@gmail.com>
2021-12-12 17:16:50 +0000

    successfully update employee role

--- src/index.js
+++ src/index.js
@@ -24,6 +24,16 @@ const generateManagerChoices = (managerFromDB) => {
   });
 };
 
+// dynamically generating employees
+const generateEmployeeChoices = (employeeFromDB) => {
+  return employeeFromDB.map((employee) => {
+    return {
+      name: employee.firstName + " " + employee.lastName,
+      value: employee.id,
+    };
+  });
+};
+
 const start = async () => {
   const db = new Db({
     host: process.envDB_HOST || "localhost",
@@ -49,51 +59,75 @@ const start = async () => {
       console.table(employees);
     }
 
+    // add an employee
     if (chosenAction === "addEmployee") {
-      // add an employee
-      if (chosenAction === "addEmployee") {
-        const role = await db.query("SELECT * FROM jobRole");
-        const employee = await db.query("SELECT * FROM employee");
+      const role = await db.query("SELECT * FROM jobRole");
+      const employee = await db.query("SELECT * FROM employee");
 
-        const employeeQuestions = [
-          {
-            type: "input",
-            message: "Please enter employee's first name:",
-            name: "firstName",
-          },
-          {
-            type: "input",
-            message: "Please enter employee's last name:",
-            name: "lastName",
-          },
-          {
-            type: "list",
-            message: "Please select a role:",
-            name: "jobRoleId",
-            choices: generateRoleChoices(role),
-          },
-          {
-            type: "list",
-            message: "Please select a Manager:",
-            name: "managerId",
-            choices: generateManagerChoices(employee),
-          },
-        ];
+      const employeeQuestions = [
+        {
+          type: "input",
+          message: "Please enter employee's first name:",
+          name: "firstName",
+        },
+        {
+          type: "input",
+          message: "Please enter employee's last name:",
+          name: "lastName",
+        },
+        {
+          type: "list",
+          message: "Please select a role:",
+          name: "jobRoleId",
+          choices: generateRoleChoices(role),
+        },
+        {
+          type: "list",
+          message: "Please select a Manager:",
+          name: "managerId",
+          choices: generateManagerChoices(employee),
+        },
+      ];
 
-        const { jobRoleId, firstName, lastName, managerId } =
-          await inquirer.prompt(employeeQuestions);
+      const { jobRoleId, firstName, lastName, managerId } =
+        await inquirer.prompt(employeeQuestions);
 
-        await db.query(
-          `INSERT INTO employee (firstName, lastName, jobRoleId, managerId) VALUES("${firstName}", "${lastName}", ${jobRoleId}, ${managerId})`
-        );
-        console.log(`Added ${firstName} ${lastName} to the database.`);
-      }
+      await db.query(
+        `INSERT INTO employee (firstName, lastName, jobRoleId, managerId) VALUES("${firstName}", "${lastName}", ${jobRoleId}, ${managerId})`
+      );
+      console.log(`Added ${firstName} ${lastName} to the database.`);
     }
 
+    // update an employee
     if (chosenAction === "updateEmployeeRole") {
-      console.table(updateEmployeeRole);
+      const employee = await db.query("SELECT * FROM employee");
+      const role = await db.query("SELECT * FROM jobRole");
+
+      const updateEmployeeQuestions = [
+        {
+          type: "list",
+          message: "Which employee would you like to update?",
+          name: "id",
+          choices: generateEmployeeChoices(employee),
+        },
+        {
+          type: "list",
+          message:
+            "What role would you like to assign to the selected employee?",
+          name: "jobRoleId",
+          choices: generateRoleChoices(role),
+        },
+      ];
+
+      const { id, jobRoleId } = await inquirer.prompt(updateEmployeeQuestions);
+
+      await db.query(
+        `UPDATE employee SET jobRoleId = ${jobRoleId} WHERE id = ${id}`
+      );
+      console.log(`Employee Role has been updated`);
     }
 
+    // add a role
     if (chosenAction === "addRoles") {
       const generateDepartmentChoices = (departmentsFromDB) => {
         return departmentsFromDB.map((department) => {
@@ -133,7 +167,7 @@ const start = async () => {
         `INSERT INTO jobRole (title, salary, departmentId) VALUES("${title}", ${salary}, ${departmentId})`
       );
     }
-
+    // view all roles
     if (chosenAction === "viewRoles") {
       const roles = await db.query(
         "SELECT jobRole.id, jobRole.title, jobRole.salary, department.name FROM jobRole JOIN department ON jobRole.departmentId = department.id ORDER BY department.name;"
@@ -141,17 +175,20 @@ const start = async () => {
       console.table(roles);
     }
 
+    // view all departments
     if (chosenAction === "viewDepartments") {
       const department = await db.query("SELECT * FROM department");
-      //displayDepartments();
       console.table(department);
     }
 
+    // add a department
     if (chosenAction === "addDepartment") {
       // ask the user the name user want to add
       //add the response in the table
       console.table("addDepartment");
     }
+
+    // exit questions
 
     if (chosenAction === "exit") {
       inProgress = false;
